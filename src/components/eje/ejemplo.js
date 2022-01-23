283269712fa26614330aae8246311d6511e0c515
Davidx144 <ndaristizabala@unal.edu.co>
2022-01-23 10:37:14 -0500

    pagina inidividual

---
+++ src/components/eje/ejemplo.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class ejemplo extends Component {
+  render() {
+    return <div>
+        <p>Hola </p>
+    </div>;
+  }
+}
--- src/components/informacion/info.js
+++ src/components/informacion/info.js
@@ -1,27 +1,25 @@
 import React, { useState/* , Component  */ } from 'react';
-import { Container } from 'react-bootstrap';
 import "./info.css"
 import BeatLoader from "react-spinners/BeatLoader"
+import Sillas from "../reservas/reservas"
+import App from "../salas/salas"
+import ejemplo from '../eje/ejemplo';
+import { Container, Row } from "react-bootstrap";
+import { /*Button, FormGroup, Form, Label,   FormText  Input,*/ Col } from 'reactstrap';
 
 //const Infopeli = props => {
 function Infopeli() {
-
-
     const [loading, setLoading] = useState(true)
     const cambiarEstado = () => {
         setTimeout(() => {
             setLoading(false)
         }, 1000);
     }
-
-
-
-
-
     if (loading) {
         cambiarEstado()
         return (
             <div>
+
                 <Container className='loading'>
                     <BeatLoader
                         size={15}
@@ -33,6 +31,21 @@ function Infopeli() {
         return (
             <Container >
                 <div className='infoPeli'>
+                    <Row>
+                        <Col sm={6}>
+                        <p>{exitoso[0].title}</p>
+                        <div >
+                        <img src={exitoso[0].img} className='imag'/>
+                        </div>
+                        </Col>
+                        <Col sm={6}>
+                    <App></App>    
+                        </Col>
+                    </Row>
+                    <Row>
+
+                    </Row>
+                    <ejemplo></ejemplo>
                     <h1>aaaaaasas</h1>
                     <p>{url}</p>
                     <p>{ids}</p>
